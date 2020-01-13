99205408f5a5642823467bcac03ded5ae67f937a
bodhibrdr <julie.gumerman@gmail.com>
2020-01-13 15:11:04 -0700

    04, 08, 10 done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,6 +11,8 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
+for num in range(5):
+    y.append(num+1)
 
 print (y)
 
@@ -18,7 +20,9 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
-
+for num in range(10):
+    y.append(num**3
+    )
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
@@ -27,6 +31,9 @@ print(y)
 a = ["foo", "bar", "baz"]
 
 y = []
+
+for x in a:
+    y.append(x.upper())
 
 print(y)
 
@@ -37,5 +44,9 @@ x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
 y = []
+for i in x:
+    new_i = int(i)
+    if new_i % 2 == 0:
+        y.append(new_i)
 
 print(y)
