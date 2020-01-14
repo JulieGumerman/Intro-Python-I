06118bb0b484b5c08f78592c1b4370985e8f2f01
bodhibrdr <julie.gumerman@gmail.com>
2020-01-14 14:58:32 -0700

    a little stretch with classes

---
+++ src/stretch.py
@@ -0,0 +1,34 @@
+class Character:
+    def __init__(self, name, species, classification, location):
+        self.species = species
+        self.classification = classification
+    
+class Hero:
+    def __init__(self, name, species, classification, location, nobility, virtue):
+        self.species = species
+        self.name = name
+        self.classification = classification
+        self.location = location
+        self.nobility = nobility
+        self.virtue = virtue
+
+    def description(self):
+        print(f"{self.name} is a {self.classification} with a nobility level of {self.nobility}")
+
+class Villain:
+    def __init__(self, name, species, classification, location, cunning, badness):
+        self.name = name
+        self.species = species
+        self.classification = classification
+        self.location = location
+        self.cunning = cunning
+        self.badness = badness
+
+    def description(self):
+        print(f"{self.name} is a {self.classification} with the darkest soul and a cunning of {self.cunning}")
+
+luke = Hero("Luke", "human", "jedi", "Tatooine", 10, 8)
+boba_fett = Villain("Boba Fett", "human", "bounty hunter", "wherever there is profit", 8, 9)
+
+boba_fett.description()
+luke.description()
